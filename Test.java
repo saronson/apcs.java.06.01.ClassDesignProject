a6f7d19141648e8af37a47158c12bfbcce21a9f5
Steve Aronson <saronson@lfschools.net>
2021-01-13 12:29:24 -0600

    Update and rename ParkingLotTest.java to Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+
+/**
+ * Test program for your class design
+ * 
+ * @author 
+ */
+public class Test extends junit.framework.TestCase
+{
+
+    public static void main(String args[])
+    {
+      
+    }
+   
+}
