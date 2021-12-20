ea1aee142c6d6a81c45e12ff0f8847af49b4de07
Steve Aronson <saronson@lfschools.net>
2021-12-20 15:26:02 -0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,17 @@ public class Test
 
     public static void main(String args[])
     {
-      
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter a string: ");
+      String str = scan.next();
+      System.out.println("You entered: " + str);
+      System.out.println("Enter an integer: ");
+      int num1 = scan.nextInt();
+      System.out.println("You entered: " + num1);
+      System.out.println("Enter a double: ");
+      double num2 = scan.nextDouble();
+      System.out.println("You entered: " + num2);
+
     }
    
 }
